e16fb4d902888ebb94117b7206e964f9242fa8fa
Gladys <doe.glad@gmail.com>
2024-02-29 02:37:33 +0000

    Add files via upload

---
+++ About.php
@@ -0,0 +1,44 @@
+<?php include('partials-front/menu.php');?>
+
+
+            <div class="about" id="About">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="Menu.html">Menu</a></li>
+                <div class="about_main">
+        
+                    <div class="image">
+                        <img src="image/Food-Plate.png">
+                    </div>
+        
+                    <div class="about_text">
+                        <h1><span>About</span>Us</h1>
+                        <br><br /><br />
+                        <p>
+                            "Welcome to Healthy Food, 
+                            where the choice for health food is an easy and fulfilling decision.
+                            At the heart of our mission is a commitment to your well-being. 
+                            We believe in the transformative power of nourishing your body with
+                             wholesome ingredients, andour dedication to quality, taste,
+                             and nutrition sets us apart.
+                            Choose Healthy Food for a journey of flavor and vitality, 
+                            where each bite is a step toward a healthier, happier you."
+                        </p>
+                    
+                        <div class="about_text">
+                            <h1>We <span>render services</span> like:</h1><br /><br /><br />
+                            <p>1. Local Dishes</p>
+                            <p>2. Continental Dishes</p>
+                            <p>3. Taking Orders</p>
+                        </div>
+
+                    </div>
+        
+                </div>
+        
+                <a href="<?php echo SITEURL; ?>Menu.php" class="about_btn">Menu</a>
+        
+            </div>
+        
+        </body>
+    </head>
+</html>
